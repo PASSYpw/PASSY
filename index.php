5188d28f33b286d07c26f183c0093862dc6be195
Sefa Eyeoglu <contact@scrumplex.net>
2017-12-13 20:46:58 +0100

    Updated logo
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- index.php
+++ index.php
@@ -178,13 +178,11 @@ if ($generalConfig["redirect_ssl"] && isset($_SERVER["HTTPS"]) && $_SERVER['HTTP
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-6 col-xs-12">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 569.2 952.76" width="50px" height="50px">
-					<path d="M500,41.24c-157.12,0-284.49,127.37-284.49,284.49h0V692.16a35.28,35.28,0,0,0,35.28,35.28h0A35.28,35.28,0,0,0,286,692.16V513.25l.57.66A283.8,283.8,0,0,0,500,610.23c157.12,0,284.49-127.37,284.49-284.49S657.11,41.24,500,41.24Zm0,497.86c-117.84,0-213.37-95.53-213.37-213.37S382.15,112.37,500,112.37s213.37,95.53,213.37,213.37S617.83,539.1,500,539.1Z"
-					      transform="translate(-215.5 -41.24)"></path>
-					<text transform="translate(0 901.39)"
-					      style="font-size:190px; font-family:Roboto-Regular, Roboto, serif; letter-spacing:-0.07em">
-						<tspan>PASSY</tspan>
-					</text>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 140.72912 185" height="50px">
+					<g fill-rule="evenodd">
+						<path d="M0 0v120l34.640625-20V60L69.28125 80l34.64258-20L0 0zm34.640625 145L0 165l34.640625 20v-40z" fill="#191919"></path>
+						<path d="M0 122.5v40l140.72913-81.25-34.64101-20z" fill="#323232"></path>
+					</g>
 				</svg>
 				<span class="text-muted hidden-xs" title="Build <?= PASSY_BUILD ?>"><?= PASSY_VERSION ?></span>
 			</div>
