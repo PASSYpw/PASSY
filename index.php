a1381f2c772204191bd608c793cbb519f83af60c
Sefa Eyeoglu <contact@scrumplex.net>
2018-03-02 14:05:40 +0100

    Move image assets to new directory
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- index.php
+++ index.php
@@ -31,12 +31,12 @@ if ($generalConfig["redirect_ssl"] && isset($_SERVER["HTTPS"]) && $_SERVER['HTTP
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="theme-color" content="#ff5722">
-	<link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="192x192" href="android-chrome-192x192.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="assets/img/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="192x192" href="assets/img/android-chrome-192x192.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon-16x16.png">
 	<link rel="manifest" href="manifest.json">
-	<link rel="mask-icon" href="safari-pinned-tab.svg" color="#ff5722">
+	<link rel="mask-icon" href="assets/img/safari-pinned-tab.svg" color="#ff5722">
 	<meta name="msapplication-TileColor" content="#ff5722">
 	<meta name="msapplication-TileImage" content="mstile-144x144.png">
 	<title><?= $customizationConfig["title"] ?></title>
