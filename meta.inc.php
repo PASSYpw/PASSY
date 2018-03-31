f375df4de3cc64c7dd03aeba4357e3945449716f
Sefa Eyeoglu <contact@scrumplex.net>
2018-03-31 13:17:13 +0200

    Update version number
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- meta.inc.php
+++ meta.inc.php
@@ -18,7 +18,7 @@
  *
  */
 
-define("PASSY_BUILD", 204);
-define("PASSY_VERSION", "2.0.4");
+define("PASSY_BUILD", 205);
+define("PASSY_VERSION", "2.0.5");
 define("PASSY_REPO", "https://github.com/PASSYpw/PASSY");
 define("PASSY_BUGTRACKER", "https://github.com/PASSYpw/PASSY/issues");
